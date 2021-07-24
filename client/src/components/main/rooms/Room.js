5b7fae2f2e2d6e1ad0228cc636e49f9367a5d177
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-07-24 06:13:40 -0500

    nearly done with room page

--- client/src/components/main/rooms/Room.js
+++ client/src/components/main/rooms/Room.js
@@ -1,24 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RoomsJSON from "../../../rooms.json";
 import BookWidget from "../../booking/BookWidget";
 import { displayIcon } from "./Icons";
-import './Room.scss'
+import "./Room.scss";
 const Room = (props) => {
+  const [show, setShow] = useState(4);
+
   const currId = props.match.params.id;
-  const currRoom = RoomsJSON.find((room) => room.url === Number(currId));
+  const currRoom = RoomsJSON.find((room) => room.url === currId);
+
+  const handleClick = () => {
+    show === 4 ? setShow(currRoom.amenities.length) : setShow(4);
+  };
+  /* const showAmenities = () => {
+    for (let i = 0; i < show; i++) {
+      return i;
+    }
+  };
+  console.log(showAmenities()); */
   return (
     <div className="Room">
       <header
         style={{
-          background:
-            `no-repeat center/cover url("/img/rooms/${currRoom.mainImage}")`,
+          background: `no-repeat center/cover url("/img/rooms/${currRoom.mainImage}")`,
         }}
       >
         <div className="header-content">
           <h2 className="alt-font">{currRoom.title}</h2>
-          <p>
-            {currRoom.titleHeader}
-          </p>
+          <p>{currRoom.titleHeader}</p>
         </div>
 
         <BookWidget />
@@ -27,42 +36,54 @@ const Room = (props) => {
         <h1 className="alt-font">{currRoom.header}</h1>
         <p>{currRoom.subHeader}</p>
       </section>
-      <section className='roomInfo'>
-        <div className='infoLeft'>
-          <div className='info'>
-<         div>
-            <h3>VIEW</h3>
-            <p>{currRoom.view}</p>
+      <section className="roomInfo">
+        <div className="infoLeft">
+          <div className="info">
+            <div>
+              <h3>VIEW</h3>
+              <p>{currRoom.view}</p>
+            </div>
+            <div>
+              <h3>SIZE</h3>
+              <p>{currRoom.size}</p>
+            </div>
+            <div>
+              <h3>OCCUPANCY</h3>
+              <p>
+                {currRoom.adults} Adults & {currRoom.children} Children
+              </p>
+            </div>
+            <div>
+              <h3>BEDDING</h3>
+              <p>{currRoom.bedding}</p>
+            </div>
           </div>
-          <div>
-            <h3>SIZE</h3>
-            <p>{currRoom.size}</p>
-          </div>
-          <div>
-            <h3>OCCUPANCY</h3>
-            <p>{currRoom.adults} Adults & {currRoom.children} Children</p>
-          </div>
-          <div>
-            <h3>BEDDING</h3>
-            <p>{currRoom.bedding}</p>
-          </div>
-          </div>
-          <div className='infoIcons'>
+          <div className="infoIcons">
             <h3>AMENITIES</h3>
-          <ul>
-             {currRoom.amenities.map((item) => 
-              <li>
-                <i className={`${displayIcon(item)} itemIcon`}></i>
-                <p>{item}</p>
+            <ul>
+              {currRoom.amenities
+                .filter((item, idx) => idx < show)
+                .map((item) => (
+                  <li key={item}>
+                    <i className={`${displayIcon(item)} itemIcon`}></i>
+                    <p>{item}</p>
+                  </li>
+                ))}
+              <li onClick={handleClick}>
+                <i
+                  className={`${
+                    show <= 4 ? "fas fa-plus" : "fas fa-minus"
+                  } itemIcon itemShow`}
+                >
+                  {show <= 4 && <span>{currRoom.amenities.length - 4}</span>}
+                </i>
               </li>
-            )}
-          </ul>
-         
+            </ul>
+          </div>
         </div>
-        </div>
-        
-        <div className='infoRight'>
-          <h2 className='alt-font'>ROOM OVERVIEW</h2>
+
+        <div className="infoRight">
+          <h2 className="alt-font">ROOM OVERVIEW</h2>
           <p>{currRoom.paragraph}</p>
         </div>
       </section>
