9c7bd9d12cea816b52adb4de2c0fab7fdf8a743f
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2022-09-03 18:44:16 -0500

    updated API url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://suay-resort-api.herokuapp.com/",
+  baseURL: "https://suay-resort-api.onrender.com/",
 }); // can set a base url here
 
 export const fetchRooms = () => API.get("/rooms");
