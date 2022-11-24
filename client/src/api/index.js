04fdcd5ab3e6c8f049cf17f41254c590f2ca12f5
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2022-11-24 17:33:16 -0600

    updated server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://suay-resort-api.onrender.com/",
+  baseURL: "https://fragile-hare-drawers.cyclic.app/",
 }); // can set a base url here
 
 export const testConnection = () => API.get("/");
